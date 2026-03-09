29846d6e7a60c32cb3b2fb18fd54fcdaf616f412
Urmilaa <urmikher@gmail.com>
2026-03-09 16:21:55 +0530

    Update AWS instance AMI and type

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
+  ami           = "ami-0b6c6ebed2801a5cb" # us-west-2
+  instance_type = "t3.micro"
   tags = {
       Name = "TF-Instance"
   }
